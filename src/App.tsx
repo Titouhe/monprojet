c8610782dc4444a87923b419455fe3cbc164a92b
Titouche <81822458+Titouhe@users.noreply.github.com>
2023-01-27 14:48:42 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { fetchBeaconApi } from './api/fetchBeaconApi';
+
 import './App.css';
 
 function App() {
+  const [beaconData, setBeaconData] = useState('');
+
+  const onChangeNewData = () => {
+    fetchBeaconApi().then((data) => {
+      setBeaconData(data.pulse.outputValue);
+    });
+  };
+
+  useEffect(() => {
+    fetchBeaconApi().then((data) => {
+      setBeaconData(data.pulse.outputValue);
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>{beaconData}</h1>
+
+      <button onClick={onChangeNewData}>Show me the last random value</button>
     </div>
   );
 }
