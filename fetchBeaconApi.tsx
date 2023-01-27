9f6be06ff8dcfaa992e8d6831ff31f55494fad3c
Titouhe <81822458+Titouhe@users.noreply.github.com>
2023-01-27 01:12:08 +0100

    Add files via upload

---
+++ fetchBeaconApi.tsx
@@ -0,0 +1,13 @@
+//https://beacon.nist.gov/beacon/2.0/chain/last/ pulse/last
+
+import { ReponseDataPulse, Pulse } from '../interfaces/reponseData';
+
+export const fetchBeaconApi = async (): Promise<ReponseDataPulse> => {
+  const response = await fetch(
+    'https://beacon.nist.gov/beacon/2.0/chain/last/pulse/last'
+  );
+
+  const pulse: ReponseDataPulse = await response.json();
+
+  return pulse;
+};
